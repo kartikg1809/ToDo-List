3862012691538014a3d87fb30406237e81ff3ab9
kartikg1809 <kartikg1809@gmail.com>
2023-12-27 11:19:36 +0530

    Completed the project

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+var items=[];
+let works=[];
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>{
+    let date=new Date();
+    const options = { weekday: 'long', day: 'numeric', month: 'long'};
+    date=date.toLocaleDateString('en-us', options);
+    res.render("index.ejs",{Type:date,tasks:items});
+});
+app.get('/work', (req, res) =>{
+    let date=new Date();
+    res.render("index.ejs",{Type:"Work List",tasks:works});
+});
+
+app.post('/', (req, res) =>{
+    var task=req.body.newTask;
+    console.log(req.body);
+    if(req.body.list==="Work List"){
+        works.push(task);
+        res.redirect("/work")
+    }
+    items.push(task);
+    res.redirect('/');
+});
+
+app.listen(3000,()=>{
+    console.log('Server listening on port 3000');
+})
